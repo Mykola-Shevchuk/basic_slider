977f5c3e832a3c21d18fbc574d79119bcf25cef2
Mykola-Shevchuk <mykola.shevchuk.ua@gmail.com>
2016-08-03 00:45:15 +0300

    add second slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,108 @@
-jQuery(document).ready(function($) {
+/*jQuery(document).ready(function($) {
 	$('.btn').on('click', function() {
 		var panelID = $(this).attr('data-panelId');
 
 		$('#' + panelID).fadeToggle();
 	});		
 });
+*/
+
+// SLIDER1
+
+$(document).ready(function($) {
+
+	//configuration
+	var width = 720;
+	var	animationSpeed = 1000;
+	var currentSlide = 1;
+	var position = 0;
+
+	// cache DOM
+	var $slider = $('#slider');
+	var $slideContainer = $slider.find('.slides');
+	var $slides = $slideContainer.find('.slide');
+
+	// navigation
+	var $left = $('.left-arrow');
+	var $right = $('.right-arrow');
 
 
-// SLIDER
+	function nav() {
+		$right.stop().on('click', function() {
+			position = Math.max(position - width * currentSlide, -width * ($slides.length - currentSlide));
+
+			$slideContainer.stop().animate({
+				marginLeft: position + 'px' 
+			}, animationSpeed, function() {
+
+			});
+		});
+
+		$left.stop().on('click', function() {
+			position = Math.min(position + width * currentSlide, 0)
+
+			$slideContainer.stop().animate({
+				marginLeft:  position + 'px'
+			}, animationSpeed, function() {
+
+			});
+		});
+	}
+
+	nav();
+});
+
+
+
+
+// SLIDER2
+
+$(document).ready(function($) {
+
+	//configuration
+	var width = 720;
+	var	animationSpeed = 1000;
+	var currentSlide = 1;
+	var position = 0;
+
+	// cache DOM
+	var $slider = $('#slider2');
+	var $slideContainer = $slider.find('.slides2');
+	var $slides = $slideContainer.find('.slide2');
+
+	// navigation
+	var $left = $('.left');
+	var $right = $('.right');
+
+
+	function nav() {
+		$right.stop().on('click', function() {
+			position = Math.max(position - width * currentSlide, -width * ($slides.length - currentSlide));
+
+			$slideContainer.stop().animate({
+				marginLeft: position + 'px' 
+			}, animationSpeed, function() {
+
+			});
+		});
+
+		$left.stop().on('click', function() {
+			position = Math.min(position + width * currentSlide, 0)
+
+			$slideContainer.stop().animate({
+				marginLeft:  position + 'px'
+			}, animationSpeed, function() {
+
+			});
+		});
+	}
+
+	nav();
+});
+
+
+
+// SLIDER2
 
 $(document).ready(function($) {
 
@@ -43,19 +138,6 @@ $(document).ready(function($) {
 		}, pause);
 	}
 
-/*	function nav() {
-		$right.on('click', function() {
-			$slideContainer.animate({
-				marginLeft: '-=' + width 
-			}, animationSpeed, function() {
-
-				if (currentSlide === $slides.length) {
-	                currentSlide = 1;
-	                $slideContainer.css('margin-left', 0);
-	            }
-			});
-		});
-	}*/
 
 	function stopSlider() {
 		clearInterval(intervalID);
@@ -63,7 +145,4 @@ $(document).ready(function($) {
 
 	$slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
 
-	startSlider();
-
-	/*nav();*/
 });
